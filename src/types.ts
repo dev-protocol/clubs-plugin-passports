73a3c6689c761a43a632cbb2872e0204dae99652
yashdesu <yash@devprotocol.xyz>
2024-09-12 13:01:49 +0530

    feat: add reindexing func and schema

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,18 @@ export type TokenURIWithId = Readonly<{
 	id: number
 	image: string
 }>
+
+export const ERROR = {
+	$400: {
+		DBNOTFOUND: 'DB not found.',
+		MISSINGDATA: 'Missing data.',
+		INVALIDREQ: 'Invalid request.',
+		CLUBSNOTFOUND: 'Clubs not found.',
+	},
+	$401: {
+		INVALIDACCESS: 'Invalid access.',
+	},
+	$500: {
+		DBERROR: 'Passport item db error',
+	},
+}
