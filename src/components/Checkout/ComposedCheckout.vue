bbdc33c164c1c2a347fee3a998111364133b5d5c
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-10-30 13:44:35 +0900

    first commit

---
+++ src/components/Checkout/ComposedCheckout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {Checkout} from '@devprotocol/clubs-core/ui/vue'
+import {TransactionForm, type ComposedItem} from '@devprotocol/clubs-plugin-payments'
+import { ref } from 'vue'
+
+const isUsingCreditCard = ref(false)
+
+</script>
+
+<template>
+	<Checkout>
+		<TransactionForm v-if="isUsingCreditCard" slot="main:transaction-form" />
+		<button
+			@click="isUsingCreditCard = true"
+			slot="after:transaction-form"
+		>Credit Card</button>
+	</Checkout>
+</template>
+
+<style scoped>
+
+</style>
