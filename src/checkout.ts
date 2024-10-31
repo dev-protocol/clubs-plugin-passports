6e30e474aa60e87f348fe90c0041cfeb22789e04
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-10-31 15:22:01 +0530

    wip: exporting an array for the composed Checkout and its props

---
+++ src/checkout.ts
@@ -0,0 +1,10 @@
+import { Checkout } from '@devprotocol/clubs-core/ui/components'
+
+
+export const checkout = [
+    {
+        payload: '',
+        componenet: Checkout,
+        props : {}
+    }
+]
