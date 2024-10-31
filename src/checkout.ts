f6981924e55964015cbff66dd9f7e095c694a954
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-10-31 15:22:22 +0530

    lint

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,10 +1,9 @@
 import { Checkout } from '@devprotocol/clubs-core/ui/components'
 
-
 export const checkout = [
-    {
-        payload: '',
-        componenet: Checkout,
-        props : {}
-    }
+	{
+		payload: '',
+		componenet: Checkout,
+		props: {},
+	},
 ]
