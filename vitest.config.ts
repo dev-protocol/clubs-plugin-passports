899a3e8493439eb236d2b5cbd2148513811ea066
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-11-12 16:26:13 +0900

    Add new ComposedCheckout astro component and update configs
    Refactored rollup and vitest configs for improved handling of output paths and noExternal dependencies. Updated package.json to align with the new output structure and ensured all necessary build steps are covered by prepack. Added ComposedCheckout.astro component to enhance checkout functionality.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-/// <reference types="vitest" />
 import { getViteConfig } from 'astro/config'
 import vue from '@vitejs/plugin-vue'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { vitePreprocess } from '@astrojs/svelte'
 
 export default getViteConfig({
+	ssr: { noExternal: [/^.*clubs-plugin.*$/] },
 	plugins: [vue(), svelte({ preprocess: [vitePreprocess()] })],
 })
