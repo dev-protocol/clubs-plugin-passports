5cbfaafe31af8764d1b6fdaa6702c01c53ee4643
Aggre <hiroyuki.aggre@gmail.com>
2025-01-27 12:02:35 +0900

    autoplay is on by default

--- src/vue/MediaEmbed.vue
+++ src/vue/MediaEmbed.vue
@@ -52,7 +52,7 @@ watch(props, ({ src }) => {
 
 	<iframe
 		v-if="type === EmbeddableMediaType.YouTube"
-		:src="`https://www.youtube.com/embed/${mediaId}?playlist=${mediaId}&autoplay=${props.autoplay ? 1 : 0}&mute=1&loop=1`"
+		:src="`https://www.youtube.com/embed/${mediaId}?playlist=${mediaId}&autoplay=${props.autoplay !== false ? 1 : 0}&mute=1&loop=1`"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		referrerpolicy="strict-origin-when-cross-origin"
 		allowfullscreen
@@ -63,7 +63,7 @@ watch(props, ({ src }) => {
 
 	<iframe
 		v-if="type === EmbeddableMediaType.YouTubeShorts"
-		:src="`https://www.youtube.com/embed/${mediaId}?playlist=${mediaId}&autoplay=${props.autoplay ? 1 : 0}&mute=1&loop=1`"
+		:src="`https://www.youtube.com/embed/${mediaId}?playlist=${mediaId}&autoplay=${props.autoplay !== false ? 1 : 0}&mute=1&loop=1`"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		referrerpolicy="strict-origin-when-cross-origin"
 		allowfullscreen
@@ -74,7 +74,7 @@ watch(props, ({ src }) => {
 
 	<iframe
 		v-if="type === EmbeddableMediaType.TikTok"
-		:src="`https://www.tiktok.com/player/v1/${mediaId}?autoplay=${props.autoplay ? 1 : 0}&controls=0&play_button=0&loop=1&timestamp=0`"
+		:src="`https://www.tiktok.com/player/v1/${mediaId}?autoplay=${props.autoplay !== false ? 1 : 0}&controls=0&play_button=0&loop=1&timestamp=0`"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		referrerpolicy="strict-origin-when-cross-origin"
 		allowfullscreen
