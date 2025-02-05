9ce23cbb230f2a69c284053f14387df19e9788bf
Aggre <hiroyuki.aggre@gmail.com>
2025-02-05 13:15:11 +0900

    upgrade to the latest way to integrate with astro+tailwind
    ref: https://docs.astro.build/en/guides/styling/#tailwind

--- .preview/preview.ssr/astro.config.mjs
+++ .preview/preview.ssr/astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config'
 import clubs from '@devprotocol/clubs-core'
 import vue from '@astrojs/vue'
 import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
 import { config } from 'dotenv'
 
 config({ path: './.env' })
 
 export default defineConfig({
 	output: 'server',
-	integrations: [clubs(), vue(), svelte(), tailwind()],
+	integrations: [clubs(), vue(), svelte()],
 })
