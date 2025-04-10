e7ce98dbed90f815195ff606f613d9a7e9fb6626
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-04-10 12:47:47 +0900

    Add support for unmuting TikTok embeds on player readiness
    This update introduces functionality to unmute TikTok video embeds when the player sends an "onReady" message. The change leverages the `tiktokRef` and ensures consistent behavior for TikTok media playback, respecting the mute state.

--- src/vue/MediaEmbed.vue
+++ src/vue/MediaEmbed.vue
@@ -21,6 +21,7 @@ const type = computed<EmbeddableMediaType | Error>(() => mediaSource(src))
 const mediaId = computed<string | undefined>(() => getMediaId(src))
 const mounted = ref(false)
 const elmX = useTemplateRef('twttr')
+const tiktokRef = useTemplateRef<HTMLIFrameElement>('tiktok-player')
 const _src = computed(() => src)
 
 const load = (src: string) => {
@@ -48,6 +49,21 @@ onMounted(() => {
 		}
 	})
 	load(src)
+
+	window.addEventListener('message', (event) => {
+		if (
+			event.data &&
+			event.data["x-tiktok-player"] &&
+			event.data.type === "onReady" &&
+			!mute &&
+			type.value === EmbeddableMediaType.TikTok
+		) {
+			tiktokRef.value?.contentWindow?.postMessage(
+				{ type: "onMute", value: false, "x-tiktok-player": true },
+				'*'
+			);
+		}
+	});
 })
 
 watch(_src, (__src) => {
@@ -87,6 +103,7 @@ watch(_src, (__src) => {
 
 	<iframe
 		v-if="type === EmbeddableMediaType.TikTok"
+		ref="tiktok-player"
 		:src="`https://www.tiktok.com/player/v1/${mediaId}?autoplay=${autoplay ? 1 : 0}&controls=0&play_button=0&loop=1&timestamp=0`"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		referrerpolicy="strict-origin-when-cross-origin"
